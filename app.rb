b973394aebeb9eeaeccd4b634c18c37cec2b5d27
chad-devos <chad.devos@kellogg.northwestern.edu>
2020-02-25 05:08:44 +0000

    Assignment 3 first attempt

--- app.rb
+++ app.rb
@@ -7,12 +7,19 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
 # enter your Dark Sky API key here
-ForecastIO.api_key = "YOUR-API-KEY"
+ForecastIO.api_key = "b1fa1a3a4bb451b1b1d1f28ef3b4371d"
 
 get "/" do
-  # show a view that asks for the location
+  view "ask"
 end
 
 get "/news" do
-  # do everything else
+    results = Geocoder.search(params["location"])
+    lat_lng = results.first.coordinates
+    @lat = lat_lng[0]
+    @lng = lat_lng[1]
+    @forecast = ForecastIO.forecast("#{@lat}","#{@lng}")
+    @current_temperature = @forecast["currently"]["temperature"]
+    @current_conditions = @forecast["currently"]["summary"]
+    view "news"
 end
---
+++ test.rb
@@ -0,0 +1,16 @@
+
+require "forecast_io"
+require "geocoder"
+ForecastIO.api_key = "b1fa1a3a4bb451b1b1d1f28ef3b4371d"
+results = Geocoder.search("paris")
+    lat_lng = results.first.coordinates
+    lat = lat_lng[0]
+    lng = lat_lng[1]
+    @forecast = ForecastIO.forecast(lat, lng)
+    @current_temperature = @forecast["currently"]["temperature"]
+    @current_conditions = @forecast["currently"]["summary"]
+
+    # puts lat_lng
+    # puts @current_temperature
+    # puts @current_conditions
+    puts @forecast
